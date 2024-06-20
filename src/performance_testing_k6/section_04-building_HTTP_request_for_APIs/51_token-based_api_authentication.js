d22ebb8d792f9c0d8fefd9dba3faca919bdfa4bf
caio abreu ferreira <abreuferr@gmail.com>
2024-06-20 12:17:21 -0300

    ajuste nos cometarios

--- src/performance_testing_k6/section_04-building_HTTP_request_for_APIs/51_token-based_api_authentication.js
+++ src/performance_testing_k6/section_04-building_HTTP_request_for_APIs/51_token-based_api_authentication.js
@@ -7,9 +7,20 @@ Options : https://www.youtube.com/watch?v=GhrvZ5nUWNg
 */
 
 /*
+# Postman - GET - https://test-api.k6.io/user/register/ - body - raw - JSON
+{
+    "username": "test_1718891539418",
+    "password": "secret"
+}
 
-- código de retorno 403 ou 404
+- Send
 
+{
+    "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTcxODk3ODYyMSwianRpIjoiMjVmOTY4OTVjMzE5NDdmYzhkNjJmY2E1YWY2MTcxMmMiLCJ1c2VyX2lkIjoxOTMxOTk5fQ.1EavHGD4Fw1g-JHJT7wnJz3hl8kk-k-gB1UgRHF4x5o",
+    "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE4ODkyNTIxLCJqdGkiOiI5ZDE5NGY5ZDcwZGY0NDU0YmI5MWRhYjFkOTM3YTlmNiIsInVzZXJfaWQiOjE5MzE5OTl9.SqCetQ6LPK5w_th4Eb4eSG_ZKOeb8fcznEu3wbtb1Ns"
+}
+
+# Postman - GET - https://test-api.k6.io/my/crocodiles/ - authorization - Auth Type: Bearer Token - Token : <ACCESS>
 */
 
 // importando bibliotecas do k6.
@@ -17,6 +28,11 @@ import http from 'k6/http';
 import { check } from 'k6';
 
 export default function () {
+
+    /*
+    Primeira parte do programa - Cadastrar um usuário e senha
+    */
+
     /*
     Armazenando usuário e senha na variável "credentials"
     A variável "credentials" será utilizada posteriormente
@@ -27,6 +43,7 @@ export default function () {
         password: 'secret_' + Date.now(),
     }
 
+    // Cadastrando usuário e senha
     http.post(
         'https://test-api.k6.io/user/register/',
         JSON.stringify(credentials),
@@ -37,6 +54,11 @@ export default function () {
         }
     );
 
+    /*
+    Segunda parte do programa - Obter o token do usuário que foi previamente cadastrado
+    */
+
+    // Enviando usuário e senha do usuário previamente cadastrado
     let res = http.post(
         'https://test-api.k6.io/auth/token/login/',
         JSON.stringify(
@@ -52,9 +74,17 @@ export default function () {
         }
     );
 
+    // Construindo a variável "accessToken" e inserindo o valor do token
     const accessToken = res.json().access;
+
+    // Exibição do token
     console.log(accessToken);
 
+    /*
+    Terceira parte do programa - executando o processo de login com o token obtido anteriormente
+    */
+
+    // Enviando o token para autenticação
     http.get(
         'https://test-api.k6.io/my/crocodiles/',
         {
@@ -64,3 +94,12 @@ export default function () {
         }
     );
 }
+
+/*
+INFO[0012] Request:
+GET /my/crocodiles/ HTTP/1.1
+Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE4ODk2NDkzLCJqdGkiOiJiNjA4NDg2ZDJiNzQ0ZWUzYjc2NTE2ZDNiOGM5OTQxZSIsInVzZXJfaWQiOjE5MzIyNjB9.qgBB1KsS3gRBgBCBOZagrr6a0a0fftfM_o3ID03oCvs
+
+INFO[0013] Response:
+HTTP/1.1 200 OK
+*/
