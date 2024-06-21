8e504b569eef05ba610fdae8811ec7b920621d70
caio abreu ferreira <abreuferr@gmail.com>
2024-06-21 10:42:48 -0300

    ajuste nos comentarios

--- src/performance_testing_k6/section_05-using_parameters_in_k6_script/61-random_think_time_sleep.js
+++ src/performance_testing_k6/section_05-using_parameters_in_k6_script/61-random_think_time_sleep.js
@@ -1,16 +1,29 @@
+/*
+Title : Random “think” time (sleep)
+Author : "Caio Abreu Ferreira" <abreuferr@gmail.com>
+Description : utilização de variáveis de ambiente
+Options : https://k6.io/docs/javascript-api/jslib/utils/randomintbetween/
+*/
+
+// importando a bibliotecas HTTP do k6.
 import http from 'k6/http';
-import { check } from 'k6';
-import { randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+import { sleep } from 'k6';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+
+export const options = {
+    // usuários virtuais
+    vus: 5,
+    // duração das interações
+    duration: '20s'
+}
 
 export default function () {
-    let res = http.get('https://test-api.k6.io/public/crocodiles/');
-    const crocodiles = res.json();
-    const crocodileIds = crocodiles.map(item => item.id);
-    const crocodileId = randomItem(crocodileIds);
-    res = http.get(`https://test-api.k6.io/public/crocodiles/${crocodileId}/`);
+    http.get('https://test.k6.io');
 
-    check(res, {
-        'status is 200': (r) => r.status === 200,
-        'crocodile has the correct id': (r) => r.json().id === crocodileId
-    });
+    // exibição
+    console.log('- VU stage -');
+
+    // o tempo entre as interações é feita de forma aleatória/randômico
+    // variando entre 1 e 5 segundos.
+    sleep(randomIntBetween(1, 5));
 }
